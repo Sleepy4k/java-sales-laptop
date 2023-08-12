1cf9d0a8b97a70cda012d8bfcbdea86d7a009322
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-08-13 01:30:11 +0700

    chore: laptop controller

--- src/com/sleepy4k/sales/laptop/controller/Laptop.java
+++ src/com/sleepy4k/sales/laptop/controller/Laptop.java
@@ -8,49 +8,6 @@ import com.sleepy4k.sales.laptop.handler.LaptopHandler;
 public class Laptop {
   private static final String FILE_PATH = "src\\com\\sleepy4k\\sales\\laptop\\data\\laptop.txt";
 
-  public static void main(String[] args) {
-    try (Scanner keyboard = new Scanner(System.in)) {
-      while (true) {
-        Helper.print("--------------------");
-        Helper.print("1. Read Laptop");
-        Helper.print("2. Add Laptop");
-        Helper.print("3. Find Laptop");
-        Helper.print("4. Update Laptop");
-        Helper.print("5. Delete Laptop");
-        Helper.print("6. Back");
-        Helper.print("Choose: ");
-
-        int choose = keyboard.nextInt();
-        keyboard.nextLine();
-
-        switch (choose) {
-          case 1:
-            Laptop.readLaptop();
-            break;
-          case 2:
-            Laptop.addLaptop();
-            break;
-          case 3:
-            Laptop.findLaptop();
-            break;
-          case 4:
-            Laptop.updateLaptop();
-            break;
-          case 5:
-            Laptop.deleteLaptop();
-            break;
-          case 6:
-            return;
-          default:
-            Helper.print("Choose 1-6");
-            break;
-        }
-      }
-    } catch (Exception e) {
-      Helper.printError(e.getMessage());
-    }
-  }
-
   public static void readLaptop() {
     String data = HandleFile.readDataFile(FILE_PATH);
 
@@ -75,45 +32,37 @@ public class Laptop {
     }
   }
 
-  public static void addLaptop() {
-    try (Scanner scanner = new Scanner(System.in)) {
-      Helper.print("Brand: ");
-      String brand = scanner.nextLine();
+  public static void addLaptop(Scanner scanner) {
+    Helper.print("Brand: ");
+    String brand = scanner.nextLine();
 
-      Helper.print("Model: ");
-      String model = scanner.nextLine();
+    Helper.print("Model: ");
+    String model = scanner.nextLine();
 
-      Helper.print("Processor: ");
-      String processor = scanner.nextLine();
+    Helper.print("Processor: ");
+    String processor = scanner.nextLine();
 
-      Helper.print("RAM: ");
-      String ram = scanner.nextLine();
+    Helper.print("RAM: ");
+    String ram = scanner.nextLine();
 
-      Helper.print("Price: ");
-      String price = scanner.nextLine();
+    Helper.print("Price: ");
+    String price = scanner.nextLine();
 
-      LaptopHandler laptop = new LaptopHandler(brand, model, processor, ram, price);
-      String data = laptop.toString();
+    LaptopHandler laptop = new LaptopHandler(brand, model, processor, ram, price);
+    String data = laptop.toString();
 
-      HandleFile.addDataFile(FILE_PATH, data);
-    } catch (Exception e) {
-      Helper.printError(e.getMessage());
-    }
+    HandleFile.addDataFile(FILE_PATH, data);
   }
 
-  public static void findLaptop() {
-    try (Scanner scanner = new Scanner(System.in)) {
-      Helper.print("Model: ");
-      String model = scanner.nextLine();
+  public static void findLaptop(Scanner scanner) {
+    Helper.print("Model: ");
+    String model = scanner.nextLine();
 
-      String data = HandleFile.findDataFile(FILE_PATH, model);
+    String data = HandleFile.findDataFile(FILE_PATH, model);
 
-      if (data == null) {
-        Helper.printError("Data not found");
-        scanner.close();
-        return;
-      }
-
+    if (data == null) {
+      Helper.printError("Data not found");
+    } else {
       String[] itemSplit = data.split(",");
 
       Helper.print("");
@@ -124,48 +73,38 @@ public class Laptop {
       Helper.print("RAM: " + itemSplit[4]);
       Helper.print("Price: " + itemSplit[5]);
       Helper.print("");
-    } catch (Exception e) {
-      Helper.printError(e.getMessage());
     }
   }
 
-  public static void updateLaptop() {
-    try (Scanner scanner = new Scanner(System.in)) {
-      Helper.print("Old Model: ");
-      String oldModel = scanner.nextLine();
+  public static void updateLaptop(Scanner scanner) {
+    Helper.print("Old Model: ");
+    String oldModel = scanner.nextLine();
 
-      Helper.print("Brand: ");
-      String brand = scanner.nextLine();
+    Helper.print("Brand: ");
+    String brand = scanner.nextLine();
 
-      Helper.print("Model: ");
-      String model = scanner.nextLine();
+    Helper.print("Model: ");
+    String model = scanner.nextLine();
 
-      Helper.print("Processor: ");
-      String processor = scanner.nextLine();
+    Helper.print("Processor: ");
+    String processor = scanner.nextLine();
 
-      Helper.print("RAM: ");
-      String ram = scanner.nextLine();
+    Helper.print("RAM: ");
+    String ram = scanner.nextLine();
 
-      Helper.print("Price: ");
-      String price = scanner.nextLine();
+    Helper.print("Price: ");
+    String price = scanner.nextLine();
 
-      LaptopHandler laptop = new LaptopHandler(brand, model, processor, ram, price);
-      String data = laptop.toString();
+    LaptopHandler laptop = new LaptopHandler(brand, model, processor, ram, price);
+    String data = laptop.toString();
 
-      HandleFile.updateDataFile(FILE_PATH, oldModel, data);
-    } catch (Exception e) {
-      Helper.printError(e.getMessage());
-    }
+    HandleFile.updateDataFile(FILE_PATH, oldModel, data);
   }
 
-  public static void deleteLaptop() {
-    try (Scanner scanner = new Scanner(System.in)) {
-      Helper.print("Model: ");
-      String model = scanner.nextLine();
+  public static void deleteLaptop(Scanner scanner) {
+    Helper.print("Model: ");
+    String model = scanner.nextLine();
 
-      HandleFile.deleteDataFile(FILE_PATH, model);
-    } catch (Exception e) {
-      Helper.printError(e.getMessage());
-    }
+    HandleFile.deleteDataFile(FILE_PATH, model);
   }
 }
